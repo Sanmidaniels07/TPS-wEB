4d0056030114d3dcd3af36ea2e3b854bd63de9b9
omowole daniel <omowoledanielsanmi@gmail.com>
2023-07-06 13:40:34 +0100

    changes

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,6 +3,7 @@ import Hero from './components/Hero'
 import SuccessStories from './components/SuccessStories'
 import SpecialiseSupport from './components/SpecialiseSupport'
 import Cookies from './components/Cookies'
+import NewsBlog from './components/NewsBlog'
 
 const Home = () => {
   return (
