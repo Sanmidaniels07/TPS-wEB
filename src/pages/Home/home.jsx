caadb42b537580e07bb42e35489c8e213984eff6
dave-lence <davidfnwudo@gmail.com>
2023-07-11 11:37:48 +0100

    fiexed cookies display

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -4,17 +4,13 @@ import SuccessStories from './components/SuccessStories'
 import SpecialiseSupport from './components/SpecialiseSupport'
 import Cookies from './components/Cookies'
 import NewsBlog from './components/NewsBlog'
-<<<<<<< HEAD:src/pages/Home/index.jsx
-=======
-
->>>>>>> 9178f5b6ea76bd9fd84758199a02ae25a3ceaccd:src/pages/Home/home.jsx
 import Partners from './components/Partners'
 import Footer from './components/Footer'
 import './home.css'
 import { useState } from 'react'
 
 const Home = () => {
-const [showCookies, setShowCookies] = useState(true)
+  const [showCookie, setShowCookies] = useState(true)
 
   return (
     <div className='relative'>
@@ -22,10 +18,10 @@ const [showCookies, setShowCookies] = useState(true)
       <Hero />
       <SpecialiseSupport />
       <SuccessStories />
-      <NewsBlog/>
-      <Partners/>
-      <Footer/>
-      <Cookies/>
+      <NewsBlog />
+      <Partners />
+      <Footer />
+      <Cookies showCookies={showCookie} onclick={() => setShowCookies(false)} />
     </div>
   )
 }
