96414e5604ea9f1c70fa3522a24c6471e2b29f7f
omowole daniel <omowoledanielsanmi@gmail.com>
2023-07-18 01:21:57 +0100

    BlogChanges

--- src/pages/Blog/components/BlogSectionFour.tsx
+++ src/pages/Blog/components/BlogSectionFour.tsx
@@ -1,8 +1,8 @@
-import guide from '../../../assets/images/guide.png'
-import blog4 from '../../../assets/images/blog4.png'
-import blog3 from '../../../assets/images/blog3.png'
+import cloudImg from '../../../assets/images/cloudImg.png'
+import cenroImg from '../../../assets/images/cenroImg.png'
+import wareImg from '../../../assets/images/wareImg.png'
 
-const BlogSectionTwo = () => {
+const BlogSectionFour = () => {
   return (
     <section id='blogSection'>
       <div className="container mx-auto justify-center items-center max-w-7xl p-6 space-y-6">
@@ -11,9 +11,9 @@ const BlogSectionTwo = () => {
       <div className="item-container inline-block cursor-pointer">
                 {/* image 1 */}
             <div className="group relative rounded mb-30 overflow-hidden md:w-1/2">
-            <img src={guide} alt="" className="w-full h-60 object-cover object-center" />
-            <p className="text-blue-600 mb-4 mt-4 font-bold text-sm">17/02/2021</p>
-            <h3 className="text-lg font-bold text-slate-950 mb-5">A Guide To VOIP Technology</h3>
+            <img src={cloudImg} alt="" className="w-full h-60 object-cover object-center" />
+            <p className="text-blue-600 mb-4 mt-4 font-bold text-sm">02/02/2021</p>
+            <h3 className="text-lg font-bold text-slate-950 mb-5">Private Cloud vs Public Cloud</h3>
             <div className="group">
             <h4 className="text-slate-950 text-lg">Learn more</h4>
            <div className="mx-0 w-20 group-hover:border-b-2 group-hover:border-sky-500"> </div>
@@ -22,9 +22,9 @@ const BlogSectionTwo = () => {
             
               {/* image 2 */}
               <div className="group relative rounded mb-30 overflow-hidden md:w-1/2">
-            <img src={blog4} alt="" className="w-full h-60 object-cover object-center" />
-            <p className="text-blue-600 mb-4 mt-4 font-bold text-sm">17/02/2021</p>
-            <h3 className="text-lg font-bold text-slate-950 mb-5">Disaster Recovery Plan Guidelines</h3>
+            <img src={cenroImg} alt="" className="w-full h-60 object-cover object-center" />
+            <p className="text-blue-600 mb-4 mt-4 font-bold text-sm">01/02/2021</p>
+            <h3 className="text-lg font-bold text-slate-950 mb-5">Censornet: Control where your company data goes with CASB</h3>
             <div className="group">
             <h4 className="text-slate-950 text-lg">Learn more</h4>
            <div className="mx-0 w-20 group-hover:border-b-2 group-hover:border-sky-500"> </div>
@@ -33,9 +33,9 @@ const BlogSectionTwo = () => {
             
               {/* image 3 */}
               <div className="group relative rounded mb-30 overflow-hidden md:w-1/2">
-            <img src={blog3} alt="" className="w-full h-60 object-cover object-center" />
-            <p className="text-blue-600 mb-4 mt-4 font-bold text-sm">17/02/2021</p>
-            <h3 className="text-lg font-bold text-slate-950 mb-5">Best Office Communication Systems</h3>
+            <img src={wareImg} alt="" className="w-full h-60 object-cover object-center" />
+            <p className="text-blue-600 mb-4 mt-4 font-bold text-sm">01/02/2021</p>
+            <h3 className="text-lg font-bold text-slate-950 mb-5">Anti-Ransomware Protection for Business</h3>
             <div className="group">
             <h4 className="text-slate-950 text-lg">Learn more</h4>
            <div className="mx-0 w-20 group-hover:border-b-2 group-hover:border-sky-500"> </div>
@@ -49,4 +49,4 @@ const BlogSectionTwo = () => {
   )
 }
 
-export default BlogSectionTwo
+export default BlogSectionFour
